2cdfcbe4f2d910c15a6a36e7da79edc682544552
Selina Kim <selinakim@fb.com>
2020-06-23 14:43:04 -0700

    Initial commit: created a button

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Sleep ");
+    }
+
+
+}
